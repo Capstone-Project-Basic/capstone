dad69779177594647568e804f72a899f3654880f
추유진 <yyeppyy1234@naver.com>
2024-06-06 18:03:25 +0900

    create map style

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart'; 
+import 'package:geocoding/geocoding.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:ui' as ui;
 
 class MapWithMyLocation extends StatefulWidget {
   const MapWithMyLocation({Key? key}) : super(key: key);
@@ -14,29 +18,56 @@ class MapWithMyLocation extends StatefulWidget {
 class Friend {
   final String name;
   final String id;
+  final String profileImage;
 
-  Friend({required this.name, required this.id});
+  Friend({required this.name, required this.id, required this.profileImage});
 }
 
 class MapWithMyLocationState extends State<MapWithMyLocation> {
-  GoogleMapController? _controller;
+  Completer<GoogleMapController> _controller = Completer();
   LatLng? _currentLocation;
   bool _isLocationServiceEnabled = false;
-  final LatLng _friendLocation = const LatLng(37.5665, 126.9780);
-  Marker? _friendMarker;
   Marker? _currentLocationMarker;
   late double latitude;
   late double longitude;
+  StreamSubscription<Position>? _positionStreamSubscription;
+  String? _mapStyle;
+  late String _mapStyleString;
 
   final LatLngBounds _koreaBounds = LatLngBounds(
     southwest: const LatLng(33.0, 124.0),
     northeast: const LatLng(39.0, 132.0),
   );
 
+  final List<Friend> _friends = [
+    Friend(name: '김동글', id: '1', profileImage: 'assets/images/profile.png'),
+    Friend(name: '이동글', id: '2', profileImage: 'assets/images/profile.png'),
+    Friend(name: '박동글', id: '3', profileImage: 'assets/images/profile.png'),
+    Friend(name: '정동글', id: '4', profileImage: 'assets/images/profile.png'),
+  ];
+
+  final Map<String, LatLng> _friendLocations = {
+    '1': const LatLng(37.5665, 126.9780),
+    '2': const LatLng(37.5765, 126.9880),
+    '3': const LatLng(37.5865, 126.9980),
+    '4': const LatLng(37.5965, 127.0080),
+  };
+
+  final Map<String, Marker> _markers = {};
+
   @override
   void initState() {
     super.initState();
     _checkLocationService();
+    _loadMapStyle().then((String style) {
+      _mapStyle = style;
+    });
+  }
+
+  @override
+  void dispose() {
+    _positionStreamSubscription?.cancel();
+    super.dispose();
   }
 
   Future<void> _checkLocationService() async {
@@ -46,7 +77,8 @@ class MapWithMyLocationState extends State<MapWithMyLocation> {
     });
     if (serviceEnabled) {
       await _requestLocationPermission();
-      _getCurrentLocation();
+      await _getCurrentLocation();
+      _updateFriendLocation();
     }
   }
 
@@ -62,60 +94,83 @@ class MapWithMyLocationState extends State<MapWithMyLocation> {
       return;
     }
   }
-  
-
 
   Future<void> _getCurrentLocation() async {
     try {
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+      setState(() {
         latitude = position.latitude;
         longitude = position.longitude;
+        _currentLocation = LatLng(latitude, longitude);
+        _addCurrentLocationMarker();
+        _moveToCurrentLocation();
+        _updateFriendLocation();
+      });
     } catch (e) {
       print(e);
+      setState(() {
+        latitude = 37.5665;
+        longitude = 126.9780;
+        _currentLocation = LatLng(latitude, longitude);
+        _addCurrentLocationMarker();
+        _moveToCurrentLocation();
+        _updateFriendLocation();
+      });
     }
-    
+  }
 
-    setState(() {
-      _currentLocation = LatLng(latitude, longitude);
-      _addCurrentLocationMarker();
-      _moveToCurrentLocation();
-    });
+  void _updateFriendLocation() {
+    for (var friend in _friends) {
+      _addFriendMarker(friend, _friendLocations[friend.id]!);
+    }
   }
 
   void _addCurrentLocationMarker() {
-    _currentLocationMarker = Marker(
-      markerId: const MarkerId('currentLocation'),
-      position: _currentLocation!,
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-    );
-    setState(() {});
-  }
-
-  void _moveToCurrentLocation() {
-    if (_controller != null && _currentLocation != null) {
-      _controller?.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: _currentLocation!,
-            zoom: 15,
-          ),
-        ),
+    if (_currentLocation != null) {
+      _currentLocationMarker = Marker(
+        markerId: const MarkerId('currentLocation'),
+        position: _currentLocation!,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
       );
+      setState(() {
+        _markers['currentLocation'] = _currentLocationMarker!;
+      });
     }
   }
 
-  void _addFriendMarker() {
-    _friendMarker = Marker(
-      markerId: const MarkerId('friend'),
-      position: _friendLocation,
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-    );
-    setState(() {});
+  void _moveToCurrentLocation() {
+    _controller.future.then((controller) {
+      if (_currentLocation != null) {
+        controller.animateCamera(
+          CameraUpdate.newLatLngZoom(
+            _currentLocation!,
+            17,
+          ),
+        );
+      }
+    });
   }
 
-  Future<void> _updateFriendLocation() async {
-    Future.delayed(const Duration(seconds: 10), _updateFriendLocation);
+  Future<Uint8List> _getResizedImage(String imagePath, int width, int height) async {
+    ByteData data = await rootBundle.load(imagePath);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width, targetHeight: height);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
+  }
+
+  Future<void> _addFriendMarker(Friend friend, LatLng position) async {
+    Uint8List imageData = await _getResizedImage(friend.profileImage, 120, 120);
+    BitmapDescriptor markerIcon = BitmapDescriptor.fromBytes(imageData);
+
+    Marker marker = Marker(
+      markerId: MarkerId(friend.id),
+      position: position,
+      icon: markerIcon,
+    );
+
+    setState(() {
+      _markers[friend.id] = marker;
+    });
   }
 
   void _onMapLongPress(LatLng location) {
@@ -123,17 +178,7 @@ class MapWithMyLocationState extends State<MapWithMyLocation> {
     _showInviteDialog(location);
   }
 
-  List<Friend> _getFriendList() {
-    return [
-      Friend(name: '김동글', id: '1'),
-      Friend(name: '이동글', id: '2'),
-      Friend(name: '박동글', id: '3'),
-      Friend(name: '정동글', id: '4'),
-    ];
-  }
-
   void _showInviteDialog(LatLng location) async {
-    List<Friend> friends = _getFriendList();
     String address = await _getAddressFromLatLng(location);
 
     showDialog(
@@ -151,13 +196,14 @@ class MapWithMyLocationState extends State<MapWithMyLocation> {
                 const Text('친구 목록:'),
                 const SizedBox(height: 8.0),
                 Column(
-                  children: friends.map((friend) {
+                  children: _friends.map((friend) {
                     return ListTile(
                       title: Text(friend.name),
                       trailing: ElevatedButton(
                         onPressed: () {
                           _handleFriendSelection(location, friend);
                           Navigator.pop(context);
+                          _showInvitationResponseDialog(friend);
                         },
                         child: const Text('초대'),
                       ),
@@ -192,18 +238,22 @@ class MapWithMyLocationState extends State<MapWithMyLocation> {
     return 'Unknown location';
   }
 
-  void _handleFriendSelection(LatLng location, Friend friend) {
+  void _handleFriendSelection(LatLng location, Friend friend) async {
     print('Invitation sent to ${friend.name} for location $location');
     _showInvitationResponseDialog(friend);
   }
 
-  void _showInvitationResponseDialog(Friend friend) {
+  Future<void> _showInvitationResponseDialog(Friend friend) async {
     showDialog(
       context: context,
       builder: (BuildContext context) {
+        Future.delayed(const Duration(seconds: 3), () {
+          Navigator.of(context).pop(true);
+        });
+
         return AlertDialog(
-          title: const Text('Invitation Response'),
-          content: Text('Waiting for ${friend.name}\'s response...'),
+          title: const Text('초대완료!'),
+          content: Text('${friend.name}에게 초대를 보냈습니다'),
           actions: [
             TextButton(
               onPressed: () {
@@ -217,38 +267,39 @@ class MapWithMyLocationState extends State<MapWithMyLocation> {
     );
   }
 
+  Future<String> _loadMapStyle() async {
+    return await rootBundle.loadString('assets/map_style.json');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // title: const Text('위치'),
         title: Text("$_currentLocation"),
       ),
       body: _isLocationServiceEnabled
-          ? Column(
-              children: [
-                Expanded(
-                  child: GoogleMap(
-                    initialCameraPosition: const CameraPosition(
-                      target: LatLng(37.5665, 126.9780),
-                      zoom: 15,
-                    ),
-                    onMapCreated: (GoogleMapController controller) {
-                      _controller = controller;
-                      _addFriendMarker();
-                      _moveToCurrentLocation();
-                      _updateFriendLocation();
-                    },
-                    myLocationEnabled: true,
-                    markers: {
-                      if (_currentLocationMarker != null) _currentLocationMarker!,
-                      if (_friendMarker != null) _friendMarker!,
-                    },
-                    onLongPress: _onMapLongPress,
-                    onCameraMove: _onCameraMove,
-                  ),
-                ),
-              ],
+          ? GoogleMap(
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(37.5665, 126.9780),
+                zoom: 15,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+                _controller.future.then((controller) {
+                  if (_mapStyle != null) {
+                    controller.setMapStyle(_mapStyle);
+                  }
+                  _addCurrentLocationMarker();
+                  _moveToCurrentLocation();
+                  for (int i = 0; i < _friends.length; i++) {
+                    _addFriendMarker(_friends[i], _friendLocations[_friends[i].id]!);
+                  }
+                });
+              },
+              myLocationEnabled: true,
+              markers: Set<Marker>.of(_markers.values),
+              onLongPress: _onMapLongPress,
+              onCameraMove: _onCameraMove,
             )
           : const Center(
               child: Text('Please enable location service.'),
@@ -262,7 +313,6 @@ class MapWithMyLocationState extends State<MapWithMyLocation> {
     }
   }
 }
-
 
 void main() {
   runApp(const MaterialApp(
